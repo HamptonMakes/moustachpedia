a2f51ce5fcf2032cf7481fa4a74834556fb082aa
Hampton Catlin <hcatlin@a002867f-d9b0-47ee-bdc3-c07d990ae810>
2012-10-30 13:24:49 -0700

    moustache

--- scripts/html.ts
+++ scripts/html.ts
@@ -10,6 +10,17 @@ $("/html") {
 
   @import sections/header.ts
   @import sections/footer.ts
+  
+  $(".//img") {
+    attribute("src") {
+      value() {
+        match(this()) {
+          with(`\/\/`) { prepend("http:") }
+        }
+        prepend("http://mustachify.me/?src=")
+      }
+    }
+  }
 
   @import mappings.ts
 }
